0a7bade12f9a20da8ff05f2ca6269fa82452bf4b
Mathieu-COSYNS <64072917+Mathieu-COSYNS@users.noreply.github.com>
2023-08-26 01:05:35 +0200

    Add maskable icons for pwa

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -8,6 +8,7 @@ import { VitePWA } from 'vite-plugin-pwa';
 import { defineConfig } from 'vitest/config';
 
 import config from './app.config.json';
+import transformLogo from './transform-logo';
 import transformManifest from './transform-manifest';
 
 export default defineConfig(({ mode }) => {
@@ -89,6 +90,10 @@ const faviconsInProd = () => {
       //   },
       // },
     }),
+    transformLogo({
+      logo: process.env.VITE_APP_LOGO,
+      backgroundColor: process.env.VITE_APP_LOGO_BACKGROUND_COLOR,
+    }),
     transformManifest({
       extraContent: {
         screenshots: config.screenshots,
@@ -96,6 +101,17 @@ const faviconsInProd = () => {
           {
             origin: process.env.VITE_APP_QR_CODE_URL,
           },
+        ],
+        icons: [
+          { src: '/assets/pwa-maskable-36x36.png', sizes: '36x36', type: 'image/png', purpose: 'maskable' },
+          { src: '/assets/pwa-maskable-48x48.png', sizes: '48x48', type: 'image/png', purpose: 'maskable' },
+          { src: '/assets/pwa-maskable-72x72.png', sizes: '72x72', type: 'image/png', purpose: 'maskable' },
+          { src: '/assets/pwa-maskable-96x96.png', sizes: '96x96', type: 'image/png', purpose: 'maskable' },
+          { src: '/assets/pwa-maskable-144x144.png', sizes: '144x144', type: 'image/png', purpose: 'maskable' },
+          { src: '/assets/pwa-maskable-192x192.png', sizes: '192x192', type: 'image/png', purpose: 'maskable' },
+          { src: '/assets/pwa-maskable-256x256.png', sizes: '256x256', type: 'image/png', purpose: 'maskable' },
+          { src: '/assets/pwa-maskable-384x384.png', sizes: '384x384', type: 'image/png', purpose: 'maskable' },
+          { src: '/assets/pwa-maskable-512x512.png', sizes: '512x512', type: 'image/png', purpose: ' maskable' },
         ],
       },
     }),
